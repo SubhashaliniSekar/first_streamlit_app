492161669c886fb5db7c4440d10c646bbc9797d7
SubhashaliniSekar <121220382+SubhashaliniSekar@users.noreply.github.com>
2022-12-22 15:25:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit 
 streamlit.title('my parents new healthy diner');
+streamlit.header('BREAKFAST MENU');
+streamlit.text('Dosa');
+streamlit.text('Onion Dosa');
+streamlit.text('chola poori');
+stremlit.text('Idle');
+streamlit.text('masala Dosa');
