123f5b17a8a1de11bca30ce1fc912a2eeed6a59b
SubhashaliniSekar <121220382+SubhashaliniSekar@users.noreply.github.com>
2023-01-05 18:25:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,26 @@
 import streamlit 
-import pandas
-streamlit.title('my parents new healthy diner');
-streamlit.header('BREAKFAST MENU');
-streamlit.text('Dosa');
-streamlit.text('Onion Dosa');
-streamlit.text('chola poori');
-streamlit.text('Idle');
-streamlit.text('masala Dosa');
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");# Let's put a pick list here so they can pick the fruit they want to include 
-my_fruit_list = my_fruit_list.set_index('Fruit');
+# import pandas
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']);
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+# streamlit.title('my parents new healthy diner');
+# streamlit.header('BREAKFAST MENU');
+# streamlit.text('Dosa');
+# streamlit.text('Onion Dosa');
+# streamlit.text('chola poori');
+# streamlit.text('Idle');
+# streamlit.text('masala Dosa');
+# streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
+# my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");# Let's put a pick list here so they can pick the fruit they want to include 
+# my_fruit_list = my_fruit_list.set_index('Fruit');
 
-# Display the table on the page.
+# # Let's put a pick list here so they can pick the fruit they want to include 
+# fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']);
+# fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-streamlit.dataframe(fruits_to_show);
+# # Display the table on the page.
+
+# streamlit.dataframe(fruits_to_show);
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
